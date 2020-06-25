d928f0d796c193cddf97129de4c64461211f34b9
yuki464 <orenomusuko555@gmail.com>
2020-06-25 17:09:22 +0900

    group user

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 gem 'devise'
+
+gem 'pry-rails'
+
 ruby '2.6.5'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence: true, uniqueness: true
+
+  has_many :group_users
+  has_many :group, through: :group_users
 end
