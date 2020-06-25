c200175fe58a576f4822b903f0ac47b37af3a1d5
yuki464 <orenomusuko555@gmail.com>
2020-06-25 18:12:34 +0900

    group.error viwes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   has_many :group_users
-  has_many :group, through: :group_users
+  has_many :groups, through: :group_users
 end
